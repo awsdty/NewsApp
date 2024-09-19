6c8cdb741e904f5cbcbb604124b1577e1e466eaf
vlad <vlad28875@gmail.com>
2024-09-19 13:57:55 +0700

    отображение главного окна и его послед обновление

--- app/src/main/java/MainWindow.kt
+++ app/src/main/java/MainWindow.kt
@@ -1,2 +1,47 @@
-class MainWindow {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import kotlinx.coroutines.delay
+
+@Composable
+fun MainWindow(viewModel: News) {
+    var firstRender by remember { mutableStateOf(true) }
+
+    if (viewModel.showWindow) {
+        if (firstRender) {
+            viewModel.startNews()
+            viewModel.changeNews()
+            firstRender = false
+        }
+        val coroutineScope = rememberCoroutineScope()
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(5000)
+                viewModel.changeNewsOne()
+            }
+        }
+        
+    }
 }
